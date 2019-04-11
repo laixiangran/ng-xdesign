c2edecc12b1d16e493bab7af4294c4202cd1731b
laixiangran <laixiangran@baidu.com>
2019-04-11 19:53:29 +0800

    添加缺失文件

---
+++ .codelyzer.js
@@ -0,0 +1,9 @@
+module.exports = {
+    predefinedDirectives: [
+        {selector: 'form', exportAs: 'ngForm'},
+
+        {selector: 'td[nb-td]', exportAs: 'nbTd'},
+        {selector: 'nb-range-datepicker', exportAs: 'nbRangeDatePicker'},
+        {selector: '[nbOverlayOrigin]', exportAs: 'nbOverlayOrigin'}
+    ]
+};
