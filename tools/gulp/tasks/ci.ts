e89415ac37118ef171dc368a8231ffa598acee99
laixiangran <laixiangran@baidu.com>
2019-04-12 15:17:35 +0800

    docs: test

--- tools/gulp/tasks/ci.ts
+++ tools/gulp/tasks/ci.ts
@@ -1,91 +1,82 @@
-import { task, src, dest } from "gulp";
-import { sequenceTask } from "../utils/sequence-task";
-import { join } from "path";
-import { config } from "../utils/config";
+import { task, src, dest } from 'gulp';
+import { sequenceTask } from '../utils/sequence-task';
+import { join } from 'path';
+import { config } from '../utils/config';
 
-const bump = require("gulp-bump");
-const gutil = require("gulp-util");
-const git = require("gulp-git");
+const bump = require('gulp-bump');
+const gutil = require('gulp-util');
+const git = require('gulp-git');
 
-const conventionalChangelog = require("gulp-conventional-changelog");
-const conventionalGithubReleaser = require("conventional-github-releaser");
+const conventionalChangelog = require('gulp-conventional-changelog');
+const conventionalGithubReleaser = require('conventional-github-releaser');
 
-const yargs = require("yargs");
+const yargs = require('yargs');
 
-const branch = "develop";
+const branch = 'develop';
 
-task(
-    "commit",
-    sequenceTask(
-        "lint",
-        "test:once",
-        "build",
-        "bump-version",
-        "changelog",
-        "commit-changes",
-        "push-changes"
-        // 'create-new-tag',
-        // 'github-release'
-    )
-);
+task('commit', sequenceTask(
+    'lint',
+    'test:once',
+    'build',
+    'bump-version',
+    'changelog',
+    'commit-changes',
+    'push-changes'
+    // 'create-new-tag',
+    // 'github-release'
+));
 
-task("bump-version", () => {
-    let bumpType = "patch";
+task('bump-version', () => {
+    let bumpType = 'patch';
 
     if (yargs.argv.major) {
-        bumpType = "major";
-    } else if (yargs.argv.minor) {
-        bumpType = "minor";
+        bumpType = 'major';
+    }
+    else if (yargs.argv.minor) {
+        bumpType = 'minor';
     }
 
-    return src("./package.json")
-        .pipe(bump({ type: bumpType }))
-        .on("error", gutil.log)
-        .pipe(dest("./"));
+    return src('./package.json')
+        .pipe(bump({type: bumpType})).on('error', gutil.log)
+        .pipe(dest('./'));
 });
 
-task("changelog", () => {
-    return src("./CHANGELOG.md", {
-        buffer: false
-    })
-        .pipe(
-            conventionalChangelog({
-                preset: "angular"
-            })
-        )
-        .pipe(dest("./"));
+task('changelog', () => {
+    return src('./CHANGELOG.md', {
+            buffer: false
+        })
+        .pipe(conventionalChangelog({
+            preset: 'angular'
+        }))
+        .pipe(dest('./'));
 });
 
-task("commit-changes", () => {
+task('commit-changes', () => {
     const commitMsg = yargs.argv.m;
-    return src(".")
+    return src('.')
         .pipe(git.add())
-        .pipe(git.commit(commitMsg || "【Prerelease】Bumped version number"));
+        .pipe(git.commit(commitMsg || '【Prerelease】Bumped version number'));
 });
 
-task("push-changes", (cb: any) => {
-    git.push("origin", branch, cb);
+task('push-changes', (cb: any) => {
+    git.push('origin', branch, cb);
 });
 
-task("create-new-tag", (cb: any) => {
-    const version = require(join(config.projectPath, "package.json")).version;
-    git.tag(version, "Created Tag for version: " + version, (error: any) => {
+task('create-new-tag', (cb: any) => {
+    const version = require(join(config.projectPath, 'package.json')).version;
+    git.tag(version, 'Created Tag for version: ' + version, (error: any) => {
         if (error) {
             cb(error);
         }
-        git.push("origin", branch, { args: "--tags" }, cb);
+        git.push('origin', branch, {args: '--tags'}, cb);
     });
 });
 
-task("github-release", (done: any) => {
-    conventionalGithubReleaser(
-        {
-            type: "oauth",
-            token: process.env.CONVENTIONAL_GITHUB_RELEASER_TOKEN
-        },
-        {
-            preset: "angular"
-        },
-        done
-    );
+task('github-release', (done: any) => {
+    conventionalGithubReleaser({
+        type: 'oauth',
+        token: process.env.CONVENTIONAL_GITHUB_RELEASER_TOKEN
+    }, {
+        preset: 'angular'
+    }, done);
 });
